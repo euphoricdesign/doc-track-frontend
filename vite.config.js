c634439244e277a168897d20e294c9044899e0d7
Merlina <noahchukita@gmail.com>
2024-09-03 18:45:04 -0300

    chore(config, styles): actualiza configuración y mejora archivos de estilos globales

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
+// vite.config.ts
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
 });
