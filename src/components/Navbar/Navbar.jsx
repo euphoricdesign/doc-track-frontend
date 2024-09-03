73ceb80fc4540581aba3df2fbae932cb68c71105
Merlina <noahchukita@gmail.com>
2024-09-03 18:42:47 -0300

    feat(style): mejora estilos del componente navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,6 @@ import "./Navbar.css";
 const Navbar = () => {
   const [isSidebarClose, setIsSidebarClose] = useState(true);
   const [isDarkMode, setIsDarkMode] = useState(false);
-
   const dispatch = useDispatch();
 
   const handleLogout = () => {
@@ -24,62 +23,50 @@ const Navbar = () => {
   const toggleSidebar = () => {
     setIsSidebarClose(!isSidebarClose);
   };
+
   return (
-    <>
-      <nav className={`sidebar ${isSidebarClose ? "close" : ""}`}>
-        <header>
-          <div className="image-text">
-            <span className="image">
-              <img src="logo.png" alt="" />
-            </span>
-
-            <div className="text logo-text">
-              <span className="name">DocTrack</span>
-            </div>
-          </div>
-          <i
-            className="bx bx-chevron-right toggle fa fa-arrow-right"
-            onClick={toggleSidebar}
-          ></i>
-        </header>
-
-        <div className="menu-bar">
-          <div className="menu">
-            <ul className="menu-links">
-              <Link to="/">
-                <li className="nav-link">
-                  <i className="fa fa-home fa-lg icon"></i>
-                  <span className="text nav-text">Home</span>
-                </li>
-              </Link>
-
-              {/* <Link to="/perfil">   
-                            <li className="nav-link">    
-                                <i className="fa fa-user fa-lg icon"></i>
-                                <span className="text nav-text">Perfil</span>
-                            </li>
-                        </Link> */}
-
-              <Link to="/appointments">
-                <li className="nav-link">
-                  <i className="fa fa-calendar fa-lg icon"></i>
-                  <span className="text nav-text">Appointments</span>
-                </li>
-              </Link>
-            </ul>
-          </div>
-
-          <div className="bottom-content">
-            <ul>
-              <li style={{ cursor: "pointer" }} onClick={handleLogout}>
-                <i className="bx bx-log-out icon fa fa-sign-out"></i>
-                <span className="text nav-text">Logout</span>
-              </li>
-            </ul>
+    <nav className={`sidebar ${isSidebarClose ? "close" : ""}`}>
+      <header>
+        <div className="image-text">
+          <span className="image">
+            <img src="logo.png" alt="" />
+          </span>
+          <div className="text logo-text">
+            <span className="name">DocTrack</span>
           </div>
         </div>
-      </nav>
-    </>
+        <i
+          className="bx bx-chevron-right toggle fa fa-arrow-right"
+          onClick={toggleSidebar}
+        ></i>
+      </header>
+      <div className="menu-bar">
+        <div className="menu">
+          <ul className="menu-links">
+            <Link to="/">
+              <li className="nav-link">
+                <i className="fa fa-home fa-lg icon"></i>
+                <span className="text nav-text">Home</span>
+              </li>
+            </Link>
+            <Link to="/appointments">
+              <li className="nav-link">
+                <i className="fa fa-calendar fa-lg icon"></i>
+                <span className="text nav-text">Appointments</span>
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <div className="bottom-content">
+          <ul>
+            <li style={{ cursor: "pointer" }} onClick={handleLogout}>
+              <i className="bx bx-log-out icon fa fa-sign-out"></i>
+              <span className="text nav-text">Logout</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
