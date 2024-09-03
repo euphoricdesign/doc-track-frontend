933359f1c48d818613f34cf3516657416c95246d
Merlina <noahchukita@gmail.com>
2024-09-03 18:41:43 -0300

    feat: crea componente ScrollToTop para hacer scroll hasta la parte superior de la página cada vez que la ruta cambie

---
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll hacia la parte superior de la página
+  }, [pathname]); // Ejecuta el efecto cada vez que cambie la ruta
+
+  return null; // No renderiza nada en el DOM
+};
+
+export default ScrollToTop;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <Provider store={store}>
         <App />
       </Provider>
