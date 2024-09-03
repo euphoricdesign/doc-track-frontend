f6eda8d2ded617344c71f8392fb1acc48048650f
Merlina <noahchukita@gmail.com>
2024-09-03 18:36:05 -0300

    feat(routing): agrega ruta /doctors/info para mostrar detalles de los médicos
    - Configura la ruta /doctors/info en el enrutador de la aplicación
    - Permite el acceso a la información detallada de cada médico

--- src/routes/AppointmentsRoutes.jsx
+++ src/routes/AppointmentsRoutes.jsx
@@ -3,18 +3,22 @@ import Home from "../views/Home/Home";
 import Appointments from "../views/Appointments/Appointments";
 import AppointmentForm from "../views/Appointments/AppointmentForm";
 import Navbar from "../components/Navbar/Navbar";
+import DoctorList from "../views/DoctorList/DoctorList";
 
 export const AppointmentsRoutes = () => {
   return (
-    <>
+    <div className="app-container">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/appointments" element={<Appointments />} />
-        <Route path="/appointments/schedule" element={<AppointmentForm />} />
-
-        <Route path="/*" element={<Navigate to="/" />} />
-      </Routes>
-    </>
+      <div className="main-content">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/doctors/info" element={<DoctorList />} />
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/appointments/schedule" element={<AppointmentForm />} />
+          <Route path="/*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
