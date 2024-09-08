fa67adffd20ee1a57c9367e0cff9bcd32adfb57a
Merlina <noahchukita@gmail.com>
2024-09-07 23:38:20 -0300

    feat: mejoras en Appointments, ajustes en modal de cancelación y actualización de tabla de turnos

--- src/views/Appointments/Appointments.jsx
+++ src/views/Appointments/Appointments.jsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Appointment from "../../components/Appointment/Appointment";
 import axios from "axios";
 
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { Link } from "react-router-dom";
 
-import "./Appointments.css";
 import { setUserAppointments } from "../../redux/userSlice";
 import { getEnvVariables } from "../../helpers/getEnvVariables";
+import { MdOutlineCancel } from "react-icons/md";
+import { MdOutlineEditCalendar } from "react-icons/md";
+import "./Appointments.css";
 
 const MySwal = withReactContent(Swal);
 
@@ -29,9 +30,6 @@ const Appointments = () => {
   };
 
   const cancelUser = async (id) => {
-    //*Lógica para cancelar un appointment
-    console.log(id);
-
     await axios.put(`${envVars.VITE_BACK_URL}/appointments/cancel/${id}`);
     await getAppointments();
   };
@@ -39,17 +37,22 @@ const Appointments = () => {
   // Función de confirmación para Sweet Alert 2
   const confirmCancel = (id) => {
     MySwal.fire({
-      title: "¿Estas seguro de cancelar el turno?",
-      text: "No vas a poder revertir esta acción",
+      title: "Are you sure you want to cancel the appointment?",
+      text: "You cannot reverse this action",
       icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: "#60D394",
+      confirmButtonColor: "#4f6dcf",
       cancelButtonColor: "#EE6055",
-      confirmButtonText: "Si, deseo cancelar mi turno",
+      confirmButtonText: "Yes, I want to cancel my appointment",
     }).then((result) => {
       if (result.isConfirmed) {
         cancelUser(id);
-        Swal.fire("Cancelado!", "Your file has been deleted.", "success");
+        Swal.fire({
+          title: "Canceled!",
+          text: "Your appointment has been cancelled.",
+          icon: "success",
+          confirmButtonColor: "#4f6dcf", // Color del botón de confirmación en el segundo modal
+        });  
       }
     });
   };
@@ -59,37 +62,45 @@ const Appointments = () => {
   }, []);
 
   return myAppointments.length ? (
-    <div className="container">
-      <h1>My appointments</h1>
-      <div className="button-container">
-        <Link to="/appointments/schedule">
-          <button className="add-appointment">Add appointment</button>
-        </Link>
+    <div className="container appointments-table">
+      <h2>My Appointments</h2>
+      <div className="flex justify-end">
+        <button className="add-appointment">
+          <Link to="/appointments/schedule">Add appointment</Link>
+          <div className="arrow-wrapper">
+            <div className="arrow"></div>
+          </div>
+        </button>
       </div>
-      <div className="table-container">
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Time</th>
-              <th>Status </th>
-              <th>Action</th>
+       <div className="appointments-table-container">
+      <table className="appointments-table">
+        <thead>
+          <tr>
+            <th>Fecha</th>
+            <th>Hora</th>
+            <th>Médico</th>
+            {/* <th>Notas</th> */}
+            <th>Estado</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {myAppointments && myAppointments.map(appointment => (
+            <tr key={appointment.id}>
+              <td>{new Date(appointment.date).toLocaleDateString()}</td>
+              <td>{appointment.time}</td>
+              <td>{appointment.doctor.name}</td>
+              {/* <td>{appointment.notes}</td> */}
+              <td>{appointment.status}</td>
+              <td>
+                <button className="action-btn" disabled onClick={() => handleEdit(appointment.id)}><MdOutlineEditCalendar className="text-[18px]" /></button>
+                <button className="action-btn" onClick={() => confirmCancel(appointment.id)}><MdOutlineCancel className="text-[18px]" /></button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {myAppointments.map((appointment) => (
-              <Appointment
-                key={appointment.id}
-                id={appointment.id}
-                date={appointment.date}
-                time={appointment.time}
-                status={appointment.status}
-                confirmCancel={confirmCancel}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
+    </div>
     </div>
   ) : (
     <>
