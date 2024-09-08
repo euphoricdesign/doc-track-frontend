d1853d68c847abbae6e44d6d035a353bead6dff5
Merlina <noahchukita@gmail.com>
2024-09-07 23:50:00 -0300

    feat: actualizar modal para ver información del doctor

--- src/components/DoctoModal/DoctorModal.jsx
+++ src/components/DoctoModal/DoctorModal.jsx
@@ -1,18 +1,65 @@
+import ReactDOMServer from "react-dom/server"
 import { useState } from "react";
-import "./DoctorModal.css";
 import { IoClose } from "react-icons/io5";
+import Toastify from "toastify-js"  
+import { ToastContent } from "../ToastContent/ToastContent";
+
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+
+import { cn } from "../../lib/utils"
+import { Button } from "../ui/button"
+import { Calendar } from "../ui/calendar"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "../ui/popover"
+
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { getEnvVariables } from "../../helpers/getEnvVariables";
+import moment from "moment";
+import "./DoctorModal.css"; 
 
 const DoctorModal = ({ doctor, onClose }) => {
   const [selectedSlot, setSelectedSlot] = useState(null);
+  const [date, setDate] = useState()
+
+  const user = useSelector((state) => state.user.user)  
+
+  const envVars = getEnvVariables()  
 
   const handleSlotClick = (slot) => {
     setSelectedSlot(slot);
   };
 
-  const handleConfirmAppointment = () => {
+  const handleConfirmAppointment = async () => {
     // Aquí puedes agregar lógica para confirmar la cita
-    alert(`Cita agendada con ${doctor.name} a las ${selectedSlot}`);
-    onClose(); // Cierra el modal después de confirmar
+    const formatedDate = moment(date).format("YYYY-MM-DD")
+
+    try {
+      const dataAppointment = { date: formatedDate, time: selectedSlot, userId: user.id, doctorId: doctor.id }
+      await axios.post(
+        `${envVars.VITE_BACK_URL}/appointments/schedule`,
+        dataAppointment,
+      ) 
+
+      const myToast = Toastify({
+        text: ReactDOMServer.renderToString(<ToastContent message={`Appointment successfully scheduled with ${doctor.name} at ${selectedSlot}`} />),
+        className: "toastify",
+        position: "left",
+        gravity: "bottom",
+        duration: 870, // Duración muy grande para simular permanencia en pantalla
+        close: false,
+        escapeMarkup: false
+      })  
+  
+      myToast.showToast()  
+      onClose(); // Cierra el modal después de confirmar
+    } catch (error) {
+      
+    }
   };
 
   return (
@@ -21,26 +68,50 @@ const DoctorModal = ({ doctor, onClose }) => {
         {/* <button className="close-button" onClick={onClose}>X</button> */}
         <IoClose className="close-button" onClick={onClose} />
         <h2>{doctor.name}</h2>
-        <p>{doctor.categories[0].name}</p>
-        <p>{doctor.description}</p>
+        <p className="font-bold">{doctor.categories[0].name}</p>
+        <p className="text-sm text-gray-400">{doctor.description}</p>
         <div className="available-times">
           <h4>Available Times</h4>
-          {/* Aquí va el código para mostrar horarios disponibles */}
-          <div className="slots-container">
-            {doctor.availableSlots.map((slot, index) => (
-              <button
-                key={index}
-                className={`slot-button ${selectedSlot === slot ? "selected" : ""}`}
-                onClick={() => handleSlotClick(slot)}
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "mb-[20px] justify-start text-left font-normal",
+                  !date && "text-muted-foreground"
+                )}
               >
-                {slot}
-              </button>
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date ? format(date, "PPP") : <span>Pick a date</span>}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+              <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                initialFocus
+                className="z-[2000]"
+              />
+            </PopoverContent>
+          </Popover>
+          <div className="grid grid-cols-3 gap-2">
+            {doctor.availableSlots.map((time, index) => (
+              <Button
+                key={index}
+                onClick={() => handleSlotClick(time)}
+                variant={selectedSlot === time ? "default" : "outline"}
+                className="w-full"
+                disabled={!date}
+              >
+                {time}
+              </Button>
             ))}
           </div>
+
+
+        
           {selectedSlot && (
-            // <button className="confirm-button" onClick={handleConfirmAppointment}>
-            //   Confirm Appointment
-            // </button>
             <button
               className="confirm-button"
               onClick={handleConfirmAppointment}
