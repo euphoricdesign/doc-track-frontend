b998490b7f9497d68b32ac3422caac209ce10a3e
Merlina <noahchukita@gmail.com>
2024-09-07 23:48:01 -0300

    feat: agrega efecto en componente App para cambiar estilos del body según la ruta

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { loadUserFromLocalStorage } from "./helpers/localStorage";
 import { setAuthenticated, setUser } from "./redux/userSlice";
+import { useLocation } from 'react-router-dom';
 
 function App() {
   const dispatch = useDispatch();
   const authenticated = useSelector((state) => state.user.authenticated);
+
+  const location = useLocation();
 
   let status = authenticated ? "authenticated" : "not-authenticated";
 
@@ -19,6 +22,19 @@ function App() {
       dispatch(setAuthenticated());
     }
   }, [dispatch]);
+
+  useEffect(() => {
+    if (location.pathname === '/auth/login' || location.pathname === '/auth/register') {
+      // Aplicar estilo específico al body cuando se está en la ruta de login
+      document.body.style.display = 'block'; 
+      document.body.style.background = 'white';
+    } else {
+      // Restablecer estilo del body para otras rutas
+      document.body.style.background = '#f3f9ff'
+      document.body.style.display = 'flex';
+    }
+  }, [location]);
+
 
   return (
     <>
